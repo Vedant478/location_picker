b15ec7222ab8a1d7964e1285f9c78839a3f270a3
Vedant Bhalchandra Shirole <55829535+Vedant478@users.noreply.github.com>
2022-09-21 16:12:18 +0530

    Update Wrapped.js

--- src/component/Wrapped.js
+++ src/component/Wrapped.js
@@ -1,1 +1,18 @@
+import React, {Children} from 'react';
+import {StatusBar} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
+export default function Wrapped({children, style}) {
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        ...style,
+      }}>
+      <StatusBar barStyle="dark-content" backgroundColor="white" />
+      {children}
+    </SafeAreaView>
+  );
+}
 
